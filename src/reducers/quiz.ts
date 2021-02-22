c5a7c75bf278fda48882dc87652dcb5814ee1dc0
[Benjamin Desole] <[benjamin.desole@gmail.com]>
2021-02-22 16:28:29 +0100

    Update quiz.ts

--- src/reducers/quiz.ts
+++ src/reducers/quiz.ts
@@ -1,11 +1,21 @@
+import { AnyAction } from "redux";
 import { TYPES } from "../actions/action-types"
+import { IquizListItem, Action } from "../models";
 
-export const QuizReducer = (state = {}, action: any) : any => {
+export interface IquizInitialState {
+    quizListItem : IquizListItem[]
+}
+
+const quizInitialState : IquizInitialState = {
+    quizListItem: []
+}
+
+export const QuizReducer = (state = quizInitialState, action: AnyAction): IquizInitialState => {
     switch(action.type) {
         case TYPES.getQuizListItems :
             return {
                 ...state,
-                quizListItem :  action.payload
+                quizListItem : (action as Action<IquizListItem[]>).payload
             }
             break;
         default:
