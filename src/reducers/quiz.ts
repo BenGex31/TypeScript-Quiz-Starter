6243395c13b1248d5bbde8fd881aca57469c33fa
[Benjamin Desole] <[benjamin.desole@gmail.com]>
2021-03-03 10:52:00 +0100

    Update quiz.ts

--- src/reducers/quiz.ts
+++ src/reducers/quiz.ts
@@ -3,11 +3,15 @@ import { TYPES } from "../actions/action-types"
 import { IquizListItem, Action } from "../models";
 
 export interface IquizInitialState {
-    quizListItem : IquizListItem[]
+    quizListItem : IquizListItem[],
+    currentQuizItemIndex : number,
+    score : number
 }
 
 const quizInitialState : IquizInitialState = {
-    quizListItem: []
+    quizListItem: [],
+    currentQuizItemIndex : 0,
+    score : 0
 }
 
 export const QuizReducer = (state = quizInitialState, action: AnyAction): IquizInitialState => {
@@ -17,7 +21,22 @@ export const QuizReducer = (state = quizInitialState, action: AnyAction): IquizI
                 ...state,
                 quizListItem : (action as Action<IquizListItem[]>).payload
             }
-            break;
+        case TYPES.incrementScore :
+            return {
+                ...state,
+                score: state.score + 1
+            }
+        case TYPES.setNextQuestion :
+            return {
+                ...state,
+                currentQuizItemIndex: state.currentQuizItemIndex + 1
+            }
+            case TYPES.restart :
+            return {
+                ...state,
+                currentQuizItemIndex : 0,
+                score : 0
+            }
         default:
             return state
     }
